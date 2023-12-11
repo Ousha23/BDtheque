fe34bb0a5cdc117b2e66cdc2d617411df423fd8c
bouchra <bouchra.laaz@gmail.com>
2023-12-11 21:00:22 +0100

    V1 liste BD + Détail BD

--- JS/listeBD.js
+++ JS/listeBD.js
@@ -1,44 +1,45 @@
-console.log("début traitement")
+
 	const SRC_IMG = "/ressources/"; // emplacement des images de l'appli
 	const SRC_ALBUM_MINI = "albumsMini/"; // emplacement des images des albums en petit
 	const SRC_DEFAULT = SRC_IMG + "noComicsMini.jpeg";
-	var affichageBD = document.getElementById("listeBD");
-	var miniImgBD = document.getElementById("miniImgBD")
+	var miniBD = document.getElementById("listeBD")
 
-
-console.log("Lecture d'un album");
 	var album = albums.get("6");
 	var serie = series.get(album.idSerie);
 	var auteur = auteurs.get(album.idAuteur);
-console.log(album.titre+" "+serie.nom+" "+auteur.nom);
 
-console.log("Liste des albums");
+	var descriptBD = document.getElementById("descriptBD");
 
+	// insère les images après formatage des données
 	albums.forEach(album => {
 	    serie = series.get(album.idSerie);
 	    auteur = auteurs.get(album.idAuteur);
-console.log(album.titre+" N°"+album.numero+" Série:"+serie.nom+" Auteur:"+auteur.nom);
-        
-		//affichageBD.innerHTML = album.titre+" N°"+album.numero+" Série:"+serie.nom+" Auteur:"+auteur.nom;
-        var nomBDalt = serie.nom + "-" + album.numero + "-" + album.titre;
-        var nomBD = nomBDalt.replace(/'|!|\?|\.|"|:|\$/g, "").replace(/à/g,"…").replace(/ê/g,'ˆ').replace(/è/g,"Š").replace(/ï/g,"‹").replace(/ô/g,"“").replace(/û/g,"–").replace(/â/g,"ƒ").replace(/(ƒg)/g,"ト").replace(/é/g,'‚').replace(/î/g,"Œ").replace(/(Šm)/g,"確").replace(/ü/g,"�").replace(/ù/g,"—").replace(/ç/g,"").replace(/(ƒn)/g,"ハ");
-		nomBD = nomBD;
+
+        var nameBDAlt = serie.nom + "-" + album.numero + "-" + album.titre;
 		
-		
-console.log(nomBDalt);
+        var nomBD = nameBDAlt.replace(/'|!|\?|\.|"|:|\$/g, "").replace(/à/g,"…").replace(/ê/g,'ˆ').replace(/è/g,"Š").replace(/ï/g,"‹").replace(/ô/g,"“").replace(/û/g,"–").replace(/â/g,"ƒ").replace(/(ƒg)/g,"ト").replace(/é/g,'‚').replace(/î/g,"Œ").replace(/(Šm)/g,"確").replace(/ü/g,"�").replace(/ù/g,"—").replace(/ç/g,"").replace(/(ƒn)/g,"ハ");
+
+		var newDivCol = document.createElement("div");
         var newImg = document.createElement("img");
-        newImg.setAttribute("src",afficheMiniAlbums(nomBD));
-		newImg.setAttribute("alt",nomBDalt +".jpg")
-console.log (newImg);
-		miniImgBD.appendChild(newImg);
+
+		newDivCol.setAttribute("class", "col-6 col-sm-4 col-md-3 col-lg-2");
         
+        newImg.setAttribute("src",showMiniAlbums(nomBD));
+		newImg.setAttribute("alt",nameBDAlt +".jpg");
+		newImg.setAttribute("class","shadow-sm p-1 mb-3 bg-white rounded");
+		
+		newDivCol.appendChild(newImg);
+		miniBD.appendChild(newDivCol);
+
+		newImg.addEventListener("click", function()  {showDetailBD(newImg, nomBD);});
 	});
+
 	/**
 	 * Permet le formatage des données en titre de l'img 
 	 * @param {string} nomFiction 
 	 * @returns le contenu src de l'img
 	 */
-    function afficheMiniAlbums (nomFiction){
+    function showMiniAlbums (nomFiction){
 		if (!serie.nom || !album.numero || !album.titre) {
 			return SRC_IMG+SRC_DEFAULT+nomFiction+".jpg";	
 		} else {
@@ -46,6 +47,81 @@ console.log (newImg);
 		}
     }
 
-console.log("Fin")
+	/**
+	 * crée le tableau du détail de la BD
+	 * @param {string} BDalt 
+	 * @param {string} title 
+	 */
+	function showDetailBD (BDalt, title) {
+		BDalt = BDalt.getAttribute("alt");
+		var tBDalt = BDalt.split("-");
+		descriptBD.classList.remove("hide");
+		miniBD.classList.add("hide");
+		recupDetailsBD(BDalt, title);
+		var row = document.createElement("tr")
+		row.setAttribute("id","row")
+		
+		for (i=0; i<tBDalt.length; i++) {
+			var cell = document.createElement("td");
+			var cellText = document.createTextNode(tBDalt[i]);
+			cell.appendChild(cellText);
+			row.appendChild(cell);
+			tDetailBD.appendChild(row);
+		}
+	  
+		document.getElementById("tBD").appendChild(tDetailBD);
 
+		createBtn();
+	}
 
+	/**
+	 * Récupère le détail de la BD En cours
+	 * @param {string} BDalt 
+	 * @param {string} title 
+	 */
+	function recupDetailsBD (BDalt, title) {
+		
+		var miniImgBD = document.createElement("img");
+		miniImgBD.setAttribute("src",showMiniAlbums(title));
+		miniImgBD.setAttribute("alt",BDalt);
+		miniImgBD.setAttribute("id", "currentBD")
+
+		var descriptBD = document.getElementById("descriptBD")
+		descriptBD.appendChild(miniImgBD);
+
+	}
+	
+	/**
+	 * Créé le boutton retour 
+	 */
+	function createBtn() {
+		var btn = document.createElement("button");
+		var t = document.createTextNode("Retour");
+		btn.appendChild(t);
+		btn.setAttribute("id", "previousBtn");
+		btn.setAttribute("class", "btnBD")
+
+		document.body.appendChild(btn);
+		btn.addEventListener("click", function()  {previous ();});
+	}
+
+	/**
+	 * Supprime les éléments affichant le détail de la BD séléctionnées
+	 */
+	function deleteElmts() {
+		var btnPrev = document.getElementById("previousBtn");
+		var imgPrev = document.getElementById("currentBD");
+		var rowPrev = document.getElementById("row");
+		btnPrev.remove();
+		imgPrev.remove();
+		rowPrev.remove();
+
+	}
+	/**
+	 * Permet le retour vers l'affichage de la liste globale des BD
+	 */
+	function previous() {
+		miniBD.classList.remove("hide");
+		descriptBD.classList.add("hide");
+		deleteElmts();
+	}
