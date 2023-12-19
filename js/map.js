18bece64741bc97ebe61f5b06867f1c0770ddff0
NF <jeaneudes.woto@hotmail.fr>
2023-12-19 17:51:24 +0100

    V2

---
+++ js/map.js
@@ -0,0 +1,71 @@
+var adherents = new Map();
+
+adherents.set("001", {
+  codeAdherent: "001",
+  nom: "Dupont",
+  prenom: "Jean",
+  email: "jean.dupont@email.com",
+  dateAdhesion: new Date("2022-12-10"),
+  login: "Dupont.ad",
+  pass: "jeandupont!",
+});
+
+adherents.set("002", {
+  codeAdherent: "002",
+  nom: "Martin",
+  prenom: "Sophie",
+  email: "sophie.martin@email.com",
+  dateAdhesion: new Date("2022-12-11"),
+  login: "Martin.so",
+  pass: "sophiemartin!",
+});
+
+adherents.set("003", {
+  codeAdherent: "003",
+  nom: "Lefevre",
+  prenom: "Pierre",
+  email: "pierre.lefevre@email.com",
+  dateAdhesion: new Date("2022-12-12"),
+  login: "Lefevre.pi",
+  pass: "pierrelefevre!",
+});
+
+adherents.set("004", {
+  codeAdherent: "004",
+  nom: "Dufour",
+  prenom: "Marie",
+  email: "marie.dufour@email.com",
+  dateAdhesion: new Date("2022-12-13"),
+  login: "Dufour.ma",
+  pass: "mariedufour!",
+});
+
+adherents.set("005", {
+  codeAdherent: "005",
+  nom: "Leroy",
+  prenom: "Lucas",
+  email: "lucas.leroy@email.com",
+  dateAdhesion: new Date("2022-12-14"),
+  login: "Leroy.lu",
+  pass: "lucasleroy!",
+});
+
+adherents.set("006", {
+  codeAdherent: "006",
+  nom: "Bertrand",
+  prenom: "Anna",
+  email: "anna.bertrand@email.com",
+  dateAdhesion: new Date("2022-12-15"),
+  login: "Bertrand.an",
+  pass: "annabertrand!",
+});
+
+
+/*
+// Affichage des résultats dans la console (facultatif)
+adherents.forEach((value, key) => {
+  console.log(key, value);
+});
+    */
+
+
