f5cd589fff921f10c178b1e0726ba7565bb45b5b
NF <jeaneudes.woto@hotmail.fr>
2023-12-19 19:26:44 +0100

    V3

--- js/liste-adherents.js
+++ js/liste-adherents.js
@@ -1,31 +1,31 @@
 function afficherAdherents() {
-    const tableIdElement = document.getElementById("tableId");
-    tableIdElement.innerHTML = ""; // Nettoie le contenu actuel
+  const tableIdElement = document.getElementById("tableId");
+  tableIdElement.innerHTML = ""; // Nettoie le contenu actuel
 
-    adherents.forEach(function (adherent, codeAdherent) {
-      // Crée une nouvelle ligne pour chaque adhérent
-      const tableRow = document.createElement("tr");
+  adherents.forEach(function (adherent, codeAdherent) {
+    // Crée une nouvelle ligne pour chaque adhérent
+    const tableRow = document.createElement("tr");
 
-      // Remplit la ligne avec les détails de l'adhérent
-      tableRow.innerHTML = `
-        <td class="col-2">${codeAdherent}</td>
-        <td class="col-2">${adherent.nom}</td>
-        <td class="col-2">${adherent.prenom}</td>
-        <td class="col-2">${adherent.email}</td>
-        <td class="col-2">
-          <button onclick="editerAdherent(${codeAdherent})">Éditer</button>
-          <button onclick="supprimerAdherent(${codeAdherent})">Supprimer</button>
-        </td>
-      `;
+    // Remplit la ligne avec les détails de l'adhérent
+    tableRow.innerHTML = `
+      <td class="col-2">${codeAdherent}</td>
+      <td class="col-2">${adherent.nom}</td>
+      <td class="col-2">${adherent.prenom}</td>
+      <td class="col-2">${adherent.email}</td>
+      <td class="col-2">
+        <button onclick="editerAdherent('${codeAdherent}')">Éditer</button>
+        <button onclick="supprimerAdherent('${codeAdherent}')">Supprimer</button>
+      </td>
+    `;
 
-      // Ajoute la ligne à la table
-      tableIdElement.appendChild(tableRow);
-    });
+    // Ajoute la ligne à la table
+    tableIdElement.appendChild(tableRow);
+  });
 }
 
 afficherAdherents();
 
-function ajouterNouvelAdherent(nom, prenom, email) {
+function ajouterAdherent(nom, prenom, email) {
   let nouvelAdherent = {
     codeAdherent: generateCodeAdherent(), // Vous devez implémenter cette fonction
     nom: nom,
@@ -35,7 +35,6 @@ function ajouterNouvelAdherent(nom, prenom, email) {
   };
 
   adherents.set(nouvelAdherent.codeAdherent, nouvelAdherent);
-  listeAdherents.push(nouvelAdherent);
 
-  return nouvelAdherent; // 
+  return nouvelAdherent;
 }
